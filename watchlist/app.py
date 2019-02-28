a28cc944c63bf41cb463697e3f497a95baea6035
qingying0 <zy1134686967@gmail.com>
2019-02-28 08:43:31 +0800

    first commit

---
+++ watchlist/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "welcome to watchlist!"
+
+@app.route("/user/<name>")
+def user_page(name):
+    return "User: %s" % name
+
+@app.route("/test")
+def test_url_for():
+    print(url_for("hello"))
+    print(url_for("user_page", name="zhanghoujia"))
+    print(url_for('test_url_for'))
+    print(url_for("test_url_for", num=2))
+    return "Test page"
